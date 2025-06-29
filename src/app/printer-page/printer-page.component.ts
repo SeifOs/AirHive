b4b31a82d44f9cafcb6f9447d0f8c00bcb8a1313
Seif <dr.saifos2001@gmail.com>
2025-06-29 08:48:44 +0300

    create some empty components

---
+++ src/app/printer-page/printer-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-printer-page',
+  imports: [],
+  templateUrl: './printer-page.component.html',
+  styleUrl: './printer-page.component.css'
+})
+export class PrinterPageComponent {
+
+}
---
+++ src/app/queue-list/queue-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-queue-list',
+  imports: [],
+  templateUrl: './queue-list.component.html',
+  styleUrl: './queue-list.component.css'
+})
+export class QueueListComponent {
+
+}
